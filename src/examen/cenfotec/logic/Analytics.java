bccb65b34bbc98783bc3383c3b6a7f1bfc4c0e3c
ArialMorales <arial.morales@gmail.com>
2019-02-27 19:55:15 -0700

    Examen: Funciones de min, max y repetitions

--- src/examen/cenfotec/logic/Analytics.java
+++ src/examen/cenfotec/logic/Analytics.java
@@ -3,9 +3,43 @@ package examen.cenfotec.logic;
 public class Analytics {
 
 	public Analytics() {
-		
+
 	}
-	
-	
+
+	public static int max(String word) {
+		int max = 0;
+		String[] splits = word.split(" ");
+		for (int i = 0; i < splits.length; i++) {
+			if (splits[i].length() >= max) {
+
+				max = splits[i].length();
+			}
+		}
+		return max;
+	}
+
+	public static int min(String word) {
+		int max = 100;
+		String[] splits = word.split(" ");
+		for (int i = 0; i < splits.length; i++) {
+			if (splits[i].length() <= max) {
+				max = splits[i].length();
+			}
+		}
+		return max;
+	}
+
+	public static int repetitions(int k, String word) {
+		int quantity = 0;
+		String[] splits = word.split(" ");
+		for(int i = 0; i < splits.length; i++) {
+			if(splits[i].length() == k) {
+				quantity += 1;
+			}
+		}
+		return quantity;
+
+	}
+
 
 }
